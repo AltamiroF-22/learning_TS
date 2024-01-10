cb0a03d6246fe221016d5f7b24ed2da89606db78
AltamiroF-22 <altamiroribeirodarocha@gmail.com>
2024-01-10 16:15:39 -0300

    first commit

---
+++ src/TypeAnotation/typeAny.ts
@@ -0,0 +1,10 @@
+function showMessage(msg: string): string {
+  return msg;
+}
+console.log(showMessage("atamiro"));
+
+function showMessage2(msg: any): any {
+  return msg;
+}
+console.log(showMessage2([2, 3, 4]));
+
---
+++ src/TypeAnotation/typeObjct.ts
@@ -0,0 +1,4 @@
+const objectA: {} = {
+  keyA: "value a",
+  keyB: "value B",
+};
