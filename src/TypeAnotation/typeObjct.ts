e0423a653cb19bb4b674948b1acabd54ca55d400
AltamiroF-22 <altamiroribeirodarocha@gmail.com>
2024-01-10 16:41:17 -0300

    second

--- src/TypeAnotation/typeObjct.ts
+++ src/TypeAnotation/typeObjct.ts
@@ -1,6 +1,16 @@
-const objectA = {
+const objectA: {
+  readonly keyA: string;
+  keyB: string;
+  keyC?: string;
+  [key: string]: unknown;
+} = {
   keyA: "value a",
   keyB: "value B",
 };
 
-console.log(objectA.keyA)
+// objectA.keyA = "new key A";
+objectA.keyB = "new key B";
+objectA.keyC = "new key C";
+objectA.keyD = "new key D";
+
+console.log(objectA);
